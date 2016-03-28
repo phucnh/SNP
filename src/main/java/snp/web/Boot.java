d5ed73f341bc564676dd1f10134fe47551b0024a
Nguyen Hong Phuc (フック） <nguyen_phuc@cyberagent.co.jp>
2016-03-29 00:51:34 +0900

    add sample code for test actor

---
+++ src/main/java/snp/web/Boot.java
@@ -0,0 +1,23 @@
+package snp.web;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import snp.service.GreeterService;
+
+public class Boot {
+    public static void main(String[] args) throws Exception {
+        final ActorSystem system = ActorSystem.create("snp-ssp");
+        final ActorRef greeter = system.actorOf(Props.create(GreeterService.class), "greater");
+        
+        try {
+            greeter.tell(new GreeterService.Greet(), null);
+        } catch (Exception e) {
+            System.err.println("Failed getting result: " + e.getMessage());
+            throw e;
+        } finally {
+            system.shutdown();
+            system.awaitTermination();
+        }
+    }
+}
