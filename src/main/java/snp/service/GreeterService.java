4e1fdda47351dbfcd9dcc6e7194201a1e390af29
Nguyen Hong Phuc (フック） <nguyen_phuc@cyberagent.co.jp>
2016-03-29 00:50:57 +0900

    add sample service

---
+++ src/main/java/snp/service/GreeterService.java
@@ -0,0 +1,16 @@
+package snp.service;
+
+import akka.actor.UntypedActor;
+
+public class GreeterService extends UntypedActor {
+    public static class Greet {}
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof Greet) {
+            System.out.print("Hello World!");
+        } else {
+            unhandled(message);
+        }
+    }
+}
